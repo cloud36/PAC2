b483746f0e3223a426da49dde8ed084b1769dd7f
ChrisB <christopherjbarton@gmail.com>
2015-10-20 16:54:11 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,13 +1,15 @@
 #Instructions
 #You should create one R script called run_analysis.R that does the following. 
 #Merges the training and the test sets to create one data set.
-#Extracts only the measurements on the mean and standard deviation for each measurement. 
-#Uses descriptive activity names to name the activities in the data set
-#Appropriately labels the data set with descriptive variable names. 
+#XXExtracts only the measurements on the mean and standard deviation for each measurement. 
+#XXUses descriptive activity names to name the activities in the data set
+#XXAppropriately labels the data set with descriptive variable names. 
 #From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
-#https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
 
+#https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+#load libraries
+library(reshape2)
 
 #set working directory and download files to working directory if they don't exist.
 
@@ -22,22 +24,47 @@ download.file(url, destfile = './data/dataset.zip')
 unzip("./data/dataset.zip", exdir = "data")
 
 #load datasets
-train <- read.csv("data/UCI HAR Dataset/train/X_train.txt", sep = "\t", header = FALSE, colClasses = "character")
-#remove double spaces
-t1 <- gsub(" ", " ", t1$V1)
-#get maximum number of column. Not equal to do breaks
-for (i in 1:nrow(train)){
-  m <- 0
-  l <- length(strsplit(as.character(train[i,]), " ")[[1]])
-  if (l > m ){
-    m <- l
-  }
-}
-#break into m columns
+features     = read.table('data/UCI HAR Dataset/features.txt',header=FALSE); #imports features.txt
+activityType = read.table('data/UCI HAR Dataset/activity_labels.txt',header=FALSE); #imports activity_labels.txt
+
+#train data
+subject_train = read.table('data/UCI HAR Dataset/train/subject_train.txt',header=FALSE); #imports subject_train.txt
+xTrain       = read.table('data/UCI HAR Dataset/train/x_train.txt',header=FALSE); #imports x_train.txt
+yTrain       = read.table('data/UCI HAR Dataset/train/y_train.txt',header=FALSE); #imports y_train.txt
+
+#Uses descriptive activity names to name the activities in the data set
+colnames(activityType)  = c('activityId','activityType');  #labels
+
+colnames(xTrain)        = features[,2]; #labels
+colnames(yTrain)        = "activityId"; #labels
+
+# read in test data
+subject_test <- read.table("data/UCI HAR Dataset/test/subject_test.txt")
+x_test <- read.table("data/UCI HAR Dataset/test/X_test.txt")
+y_test <-  read.table("data/UCI HAR Dataset/test/Y_test.txt")
+
+colnames(x_test)        = features[,2];  #labels
+colnames(y_test)        = "activityId"; #labels
 
 
+# merge train data
+train_Data = cbind(yTrain,xTrain);
+test_Data = cbind(y_test,x_test);
+#extract sd, mu
+test_data <- test_Data[,grepl("std|mean", colnames(test_Data))]
+train_data <- train_Data[,grepl("std|mean", colnames(train_Data))]
 
-#t1$V2 <- lapply(strsplit(as.character(t1$V1), " "), "[[", 1)
-#df$newColumn2 <- lapply(strsplit(as.character(df$originalColumn), "and"), "[", 2)
+#add subject ids
+colnames(subject_train)  = "subjectId";  #labels
+colnames(subject_test)  = "subjectId"; #labels
+test_data <- cbind(subject_test, test_data)
+train_data <- cbind(subject_train, train_data)
 
-test <- read.csv("data/UCI HAR Dataset/test/X_test.txt", , sep = "\t", header = FALSE)
+#colnames(subject_train)  = "subjectId";  #labels
+#colnames(subject_test)  = "subjectId"; #labels
+
+str(train_data)
+str(test_data)
+
+#merge datasets
+full_data <- rbind(train_data, test_data)
