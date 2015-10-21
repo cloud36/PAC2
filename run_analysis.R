cff5e6340e7e4fed69f0782183bc51c2dcc1248a
ChrisB <christopherjbarton@gmail.com>
2015-10-20 20:27:46 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -10,6 +10,7 @@
 #https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
 #load libraries
 library(reshape2)
+library(data.table)
 
 #set working directory and download files to working directory if they don't exist.
 
@@ -24,19 +25,18 @@ download.file(url, destfile = './data/dataset.zip')
 unzip("./data/dataset.zip", exdir = "data")
 
 #load datasets
-features     = read.table('data/UCI HAR Dataset/features.txt',header=FALSE); #imports features.txt
-activityType = read.table('data/UCI HAR Dataset/activity_labels.txt',header=FALSE); #imports activity_labels.txt
+features <- read.table('data/UCI HAR Dataset/features.txt',header=FALSE);
+activityType <- read.table('data/UCI HAR Dataset/activity_labels.txt',header=FALSE);
 
 #train data
-subject_train = read.table('data/UCI HAR Dataset/train/subject_train.txt',header=FALSE); #imports subject_train.txt
-xTrain       = read.table('data/UCI HAR Dataset/train/x_train.txt',header=FALSE); #imports x_train.txt
-yTrain       = read.table('data/UCI HAR Dataset/train/y_train.txt',header=FALSE); #imports y_train.txt
+subject_train <- read.table('data/UCI HAR Dataset/train/subject_train.txt',header=FALSE); #imports subject_train.txt
+x_train <- read.table('data/UCI HAR Dataset/train/x_train.txt',header=FALSE); #imports x_train.txt
+y_train <- read.table('data/UCI HAR Dataset/train/y_train.txt',header=FALSE); #imports y_train.txt
 
 #Uses descriptive activity names to name the activities in the data set
-colnames(activityType)  = c('activityId','activityType');  #labels
-
-colnames(xTrain)        = features[,2]; #labels
-colnames(yTrain)        = "activityId"; #labels
+colnames(activityType) <- c('activityId','activityType');  #labels
+colnames(x_train) <- features[,2]; #labels
+colnames(y_train) <- "activityId"; #labels
 
 # read in test data
 subject_test <- read.table("data/UCI HAR Dataset/test/subject_test.txt")
@@ -46,25 +46,24 @@ y_test <-  read.table("data/UCI HAR Dataset/test/Y_test.txt")
 colnames(x_test)        = features[,2];  #labels
 colnames(y_test)        = "activityId"; #labels
 
-
-# merge train data
-train_Data = cbind(yTrain,xTrain);
-test_Data = cbind(y_test,x_test);
 #extract sd, mu
-test_data <- test_Data[,grepl("std|mean", colnames(test_Data))]
-train_data <- train_Data[,grepl("std|mean", colnames(train_Data))]
+x_train <- x_train[,grepl("std|mean", colnames(x_train))]
+x_test <- x_test[,grepl("std|mean", colnames(x_test))]
+
+train_data = cbind(y_train,x_train);
+test_data = cbind(y_test,x_test);
 
 #add subject ids
 colnames(subject_train)  = "subjectId";  #labels
 colnames(subject_test)  = "subjectId"; #labels
+
 test_data <- cbind(subject_test, test_data)
 train_data <- cbind(subject_train, train_data)
 
-#colnames(subject_train)  = "subjectId";  #labels
-#colnames(subject_test)  = "subjectId"; #labels
+# change acitivity labels to names
 
-str(train_data)
-str(test_data)
 
 #merge datasets
 full_data <- rbind(train_data, test_data)
+full_dt <- data.table(full_data)
+full_dt <- full_dt[,lapply(.SD,mean),by=list(subjectId,activityId)]
