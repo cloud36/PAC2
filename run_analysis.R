8684feaa1e0879822a3b53361f320b9ecafdce93
ChrisB <christopherjbarton@gmail.com>
2015-10-25 14:08:07 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,9 @@
 #Instructions
 #You should create one R script called run_analysis.R that does the following. 
 #Merges the training and the test sets to create one data set.
-#XXExtracts only the measurements on the mean and standard deviation for each measurement. 
-#XXUses descriptive activity names to name the activities in the data set
-#XXAppropriately labels the data set with descriptive variable names. 
+#Extracts only the measurements on the mean and standard deviation for each measurement. 
+#Uses descriptive activity names to name the activities in the data set
+#Appropriately labels the data set with descriptive variable names. 
 #From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 
@@ -13,16 +13,15 @@ library(reshape2)
 library(data.table)
 
 #set working directory and download files to working directory if they don't exist.
-
 #create data directory if it doesn't exist
+#and download, unzip data
 if (!file.exists("data")) {
   dir.create("data")
+  #download data
+  url <- "http://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  download.file(url, destfile = './data/dataset.zip')
+  unzip("./data/dataset.zip", exdir = "data")
 }
-
-#download data
-url <- "http://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(url, destfile = './data/dataset.zip')
-unzip("./data/dataset.zip", exdir = "data")
 
 #load datasets
 features <- read.table('data/UCI HAR Dataset/features.txt',header=FALSE);
@@ -60,18 +59,21 @@ colnames(subject_test)  = "subjectId"; #labels
 test_data <- cbind(subject_test, test_data)
 train_data <- cbind(subject_train, train_data)
 
-# change acitivity labels to names
-
-
 #merge datasets
 full_data <- rbind(train_data, test_data)
 full_dt <- data.table(full_data)
 full_dt <- full_dt[,lapply(.SD,mean),by=list(subjectId,activityId)]
+#change activityId to descriptive string
+full_dt$activityId <- as.factor(full_dt$activityId)
+levels(full_dt$activityId)[levels(full_dt$activityId)=="1"] <- "walking"
+levels(full_dt$activityId)[levels(full_dt$activityId)=="2"] <- "walking_upstairs"
+levels(full_dt$activityId)[levels(full_dt$activityId)=="3"] <- "walking_downstairs"
+levels(full_dt$activityId)[levels(full_dt$activityId)=="4"] <- "sitting"
+levels(full_dt$activityId)[levels(full_dt$activityId)=="5"] <- "standing"
+levels(full_dt$activityId)[levels(full_dt$activityId)=="6"] <- "laying"
 
 #write table
-write.table(full_dt, "tidy.txt")
-
-
+write.table(full_dt, "./data/tidy.txt")
 
 
 
